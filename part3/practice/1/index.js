6d14cfb420791be97f39be22b4c80ec4fa7bf700
ark aryan <arkaryangit@gmail.com>
2021-06-21 14:42:30 +0530

    added vs rest_client with adding and deleting functionality

--- part3/practice/1/index.js
+++ part3/practice/1/index.js
@@ -1,9 +1,9 @@
-const e = require('express')
 const express = require('express')
 const app = express()
 
 //imports express and assign it to variable app
 
+app.use(express.json())
 
 
 let notes = [
@@ -27,6 +27,12 @@ let notes = [
     }
   ]
 
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -50,8 +56,38 @@ app.get('/api/notes/:id', (request, response) => {
   
 })
 
+
 //for adding a route parameter (eg id), we add it through params
 //The captured values are populated in the req.params object
+
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if(!body.content){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response)=>{
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
